27c4b22d16e5b33aff30268ebd73fdb4e551f2a3
Vladimir Cetkovic <vladimir.cetkovic@gmail.com>
2013-09-03 23:16:15 +0200

    finished static pages part (3)

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About us'" do
+      visit '/static_pages/about'
+      page.should have_content('About us')
+    end
+  end
+
+  describe "Title test" do
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+  			:text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+end
